512efad60876e00653b55c39ffcbb20e8c6fb990
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-28 15:46:51 -0700

    Base add question button added to QA

--- Client/src/Components/QA.jsx
+++ Client/src/Components/QA.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import QuestionSearch from './QA/QuestionSearch.jsx'
-import QuestionList from './QA/QuestionList.jsx'
-import MoreQuestionsButton from './QA/MoreQuestionsButton.jsx'
+import QuestionSearch from './QA/QuestionSearch.jsx';
+import QuestionList from './QA/QuestionList.jsx';
+import MoreQuestionsButton from './QA/MoreQuestionsButton.jsx';
+import AddQuestionButton from './QA/AddQuestionButton.jsx';
 
 class QA extends React.Component {
   constructor(props) {
@@ -18,6 +19,7 @@ class QA extends React.Component {
         <QuestionSearch/>
         <QuestionList/>
         <MoreQuestionsButton/>
+        <AddQuestionButton/>
       </div>
     )
   }
---
+++ Client/src/Components/QA/AddQuestionButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class AddQuestionButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (<button id='AddQuestion'></button>)
+  }
+}
+
+export default AddQuestionButton;
