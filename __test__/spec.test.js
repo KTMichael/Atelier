c43ddc2f38878242740d686752088453279a3a5a
Brett Roberts <brettr2010@gmail.com>
2021-10-27 19:26:01 -0500

    we have our first passing test

---
+++ __test__/spec.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// // libary to mock out a server
+// // import {rest} from 'msw';
+// // import {setupServer} from 'msw/node';
+
+// // library to test react
+// import {render, fireEvent, waitFor, screen} from '@testing-library/react';
+// import '@testing-library/jest-dom';
+
+// // import component I want to test
+// import App from '../Client/Components/App.jsx';
+
+describe("This test that should pass", () => {
+  test("pass please", () => {
+    expect("link").toEqual("link");
+  });
+});
