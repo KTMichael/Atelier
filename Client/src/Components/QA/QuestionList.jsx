ade9a894af5c833b44e7011e12277c4426c94da1
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-29 11:27:27 -0700

    internal server routing for qa questions

--- Client/src/Components/QA/QuestionList.jsx
+++ Client/src/Components/QA/QuestionList.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 class QuestionList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      questions: {}
     }
   }
 
+  componentDidMount() {
+    //TODO: Remove this dirty direct get request, should be done through the server router
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions/?product_id=42366')
+    .then((result) => { this.setState({questions: result})})
+  }
+
   render() {
-    return (<ul></ul>)
+    return (
+    <ul>
+
+    </ul>
+    )
   }
 }
 
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,22 @@
+const api = require('./AtelierAPI');
+
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(express.static(__dirname + '/../Client/dist'));
+
+app.get('/qa/questions/', (req, res) => {
+  console.log(req.query.product_id);
+  api.getQuestions(req.query.product_id, (err, results) => {
+    console.log(err);
+    if (!err) {
+      res.status(200).send(results);
+    } else {
+      res.sendStatus(404)
+    }
+  });
+});
 
 
 let port = 3000;
