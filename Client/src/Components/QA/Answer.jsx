9bf4a680de702001bb52ec600098bc8bd5eb0af3
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-29 15:25:13 -0700

    Added moment library, Added username and date to answers

--- Client/src/Components/QA/Answer.jsx
+++ Client/src/Components/QA/Answer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 class Answer extends React.Component {
   constructor(props) {
@@ -7,7 +8,10 @@ class Answer extends React.Component {
 
   render() {
     return (
-      <p className ='question'>A: {this.props.data.body}</p>
+      <>
+        <p className='question'>A: {this.props.data.body}</p>
+        <p>by {this.props.data.answerer_name}, {moment(this.props.data.date).format('MMMM DD, YYYY')}</p>
+      </>
     )
   }
 }
