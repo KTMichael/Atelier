76a05871962f5b1b550dde94855e5248ada3c550
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-29 16:20:19 -0700

    answers from seller sort to top

--- Client/src/Components/QA/Question.jsx
+++ Client/src/Components/QA/Question.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Answer from './Answer.jsx'
 
+let sortParam = (firstAns, secondAns) => {
+  if (firstAns.answerer_name === 'Seller' && secondAns.answerer_name !== 'Seller') {
+    return -1;
+  }
+
+  if (secondAns.answerer_name === 'Seller' && firstAns.answerer_name !== 'Seller') {
+    return 1;
+  }
+
+  return secondAns.helpfulness - firstAns.helpfulness;
+}
+
 class Question extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      answers: Object.values(this.props.data.answers).sort((firstAns, secondAns) => {return secondAns.helpfulness - firstAns.helpfulness})
+      answers: Object.values(this.props.data.answers).sort(sortParam)
     }
   }
 
