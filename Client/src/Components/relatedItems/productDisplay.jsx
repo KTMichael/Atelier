42799f0a0250bb69c3a0c7a072d873a6e5e89ea6
Charlie Paik <chpaik88@gmail.com>
2021-10-30 23:48:59 -0700

    added styling to product display

--- Client/src/Components/relatedItems/productDisplay.jsx
+++ Client/src/Components/relatedItems/productDisplay.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ProductDisplay() {
   return (
     <div id='product-display'>
-
+      <img src="https://www.w3schools.com/images/w3schools_green.jpg"/>
     </div>
   )
 }
--- Client/src/Components/relatedItems/scrollableProductContainer.jsx
+++ Client/src/Components/relatedItems/scrollableProductContainer.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 import ProductDisplay from './productDisplay.jsx';
 import { testContext } from './context.js';
 
@@ -6,12 +7,37 @@ import { testContext } from './context.js';
 function scrollableProductContainer() {
   const { relatedProducts, outfits } = useContext(testContext);
 
+  const nextSlide = () => {
+  }
+
+  const prevSlide = () => {
+  }
+
 
   return (
     <div className='scrolling-product-container'>
-      <button id='left-scroll' type='button'>Left Scroll</button>
-      {relatedProducts.map(product => {
-        return <ProductDisplay />
+      {/* <FaChevronLeft id='left-button' onClick={prevSlide}/>
+      <FaChevronRight id='right-button' onClick={nextSlide}/> */}
+      {relatedProducts.map( (product, index) =>{
+        return (
+          <testContext.Provider value={{product}}>
+            <ProductDisplay />
+          </testContext.Provider>
+        )
+      })}
+      {relatedProducts.map( (product, index) =>{
+        return (
+          <testContext.Provider value={{product}}>
+            <ProductDisplay />
+          </testContext.Provider>
+        )
+      })}
+      {relatedProducts.map( (product, index) =>{
+        return (
+          <testContext.Provider value={{product}}>
+            <ProductDisplay />
+          </testContext.Provider>
+        )
       })}
     </div>
   )
