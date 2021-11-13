e051ae5599e9b3653fff67a46cbdfa6e2ccd87e6
Edwin-Baranov <edwin.baranov@gmail.com>
2021-11-13 09:39:02 -0800

    hooks conversion

--- Client/src/Components/QA/QuestionList.jsx
+++ Client/src/Components/QA/QuestionList.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Question from './Question.jsx';
+import MoreQuestionsButton from './MoreQuestionsButton.jsx';
 
-class QuestionList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function QuestionList(props) {
+  const [listCount, setListCount] = useState(4);
 
-  render() {
-    return (
-    <div id='questionList'>
-      {this.props.questions.map((question, index) => <Question key = {`question ${index}`} data={question} />)}
-    </div>
-    )
-  }
+  return (
+    <>
+      <div id='questionList'>
+        {this.props.questions.map((question, index) => <Question key={`question ${index}`} data={question} />)}
+      </div>
+
+      <MoreQuestionsButton />
+    </>
+  )
 }
 
 export default QuestionList;
--- Client/src/Components/QA/index.jsx
+++ Client/src/Components/QA/index.jsx
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import QuestionSearch from './QuestionSearch.jsx';
 import QuestionList from './QuestionList.jsx';
-import MoreQuestionsButton from './MoreQuestionsButton.jsx';
 import AddQuestionButton from './AddQuestionButton.jsx';
 
 function QA(props) {
   const [search, setSearchText] = useState('');
   const [questionArr, setQuestionArr] = useState([]);
-  const [listCount, setListCount] = useState(2);
 
 
   useEffect(() => {
@@ -21,7 +19,6 @@ function QA(props) {
       <h1>Questions & Answers</h1>
       <QuestionSearch value={search} change={setSearchText} />
       <QuestionList questions={questionArr} />
-      <MoreQuestionsButton />
       <AddQuestionButton />
     </div>
   )
